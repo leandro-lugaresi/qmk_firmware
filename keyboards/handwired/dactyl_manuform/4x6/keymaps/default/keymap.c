26a9ff61313459fd7c070c8dd4f5361750aa9f84
Leandro Lugaresi <leandrolugaresi92@gmail.com>
2021-12-19 09:17:45 -0300

    dactyl manuform modifications

--- keyboards/handwired/dactyl_manuform/4x6/keymaps/default/keymap.c
+++ keyboards/handwired/dactyl_manuform/4x6/keymaps/default/keymap.c
@@ -22,15 +22,12 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base (qwerty)
      * +-----------------------------------------+                             +-----------------------------------------+
-     * | ESC  |   q  |   w  |   e  |   r  |   t  |                             |   y  |   u  |   i  |   o  |   p  |      |
+     * | ESC  |   q  |   w  |   e  |   r  |   t  |                             |   y  |   u  |   i  |   o  |   p  |  -_  |
      * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
-     * | TAB  |   a  |   s  |   d  |   f  |   g  |                             |   h  |   j  |   k  |   l  |   ;  |      |
+     * | TAB  |   a  |   s  |   d  |   f  |   g  |                             |   h  |   j  |   k  |   l  |   ;  |  '"  |
      * |------+------+------+------+------+------|                             |------+------+------+------+------+------|
-     * | SHFT |   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  |   /  |      |
+     * | SHFT |   z  |   x  |   c  |   v  |   b  |                             |   n  |   m  |   ,  |   .  |   /  |   \   |
      * +------+------+------+------+-------------+                             +-------------+------+------+------+------+
-     *               |  [   |   ]  |                                                         |      |      |
-     *               +-------------+-------------+                             +-------------+-------------+
-     *                             |      |      |                             |      |      |
      *                             |------+------|                             |------+------|
      *                             |      |      |                             |      |      |
      *                             +-------------+                             +-------------+
@@ -44,29 +41,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_MINS,
         KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS,
-                          KC_LBRC, KC_RBRC,                                                KC_PLUS, KC_EQL,
-                                            RAISE,   KC_SPC,             KC_ENT,  LOWER,
-                                            KC_TAB,  KC_HOME,            KC_END,  KC_DEL,
-                                            KC_BSPC, KC_GRV,             KC_LGUI, KC_LALT
+                                            RAISE,   KC_SPC,             KC_ENT,    LOWER,
+                                            KC_BSPC, KC_LBRC,            KC_RBRC,  KC_RSFT,
+                                            KC_LCTL, KC_LALT,            KC_RALT, KC_LGUI
     ),
 
     [_LOWER] = LAYOUT(
-        _______, _______, _______, _______, _______, KC_LBRC,             KC_RBRC, KC_P7,   KC_P8,   KC_P9,   RESET,   KC_PLUS,
-        _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END , KC_LPRN,             KC_RPRN, KC_P4,   KC_P5,   KC_P6,   KC_MINS, KC_PIPE,
-        _______, _______, _______, _______, _______, _______,             _______, KC_P1,   KC_P2,   KC_P3,   KC_EQL,  KC_UNDS,
-                          _______, KC_PSCR,                                                 _______, KC_P0,
+        _______,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                KC_6,    KC_7,   KC_8,   KC_9,      KC_0, KC_PLUS,
+        _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END , KC_LPRN,             KC_RPRN,    KC_4,   KC_5,   KC_6,   KC_MINS, KC_PIPE,
+        _______, _______, _______, _______, _______, KC_GRV ,             KC_0   ,    KC_1,   KC_2,   KC_3,    KC_EQL, KC_UNDS,
                                             _______, _______,             _______, _______,
                                             _______, _______,             _______, _______,
                                             _______, _______,             _______, _______
     ),
 
     [_RAISE] = LAYOUT(
-        _______, RESET,   _______, _______, _______, KC_LBRC,             KC_RBRC, _______, KC_NLCK, KC_INS,  KC_SLCK, KC_MUTE,
-        _______, KC_LEFT, KC_UP  , KC_DOWN, KC_RGHT, KC_LPRN,             KC_RPRN, KC_MPRV, KC_MPLY, KC_MNXT, _______, KC_VOLU,
-        _______, _______, _______, _______, _______, _______,             _______, _______, _______, _______, _______, KC_VOLD,
-                          _______, _______,                                                 KC_EQL,  _______,
+        _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,             KC_F6  ,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
+        _______, KC_LEFT, KC_UP  , KC_DOWN, KC_RGHT, KC_LPRN,             KC_RPRN, KC_MPRV, KC_MPLY, KC_MNXT, KC_MUTE, KC_VOLU,
+        _______, _______, _______, _______, _______, KC_LBRC,             KC_RBRC, _______, _______, _______, KC_SLCK, KC_VOLD,
                                             _______, _______,             _______, _______,
-                                            _______, _______,             _______, _______,
+                                            _______, _______,             _______, KC_DEL ,
                                             _______, _______,             _______, _______
     )
 };
